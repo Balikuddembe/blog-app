3553ea7083decf7b9cc9cea44f996827aacb383e
Balikuddembe <balixemma@gmail.com>
2023-05-23 18:42:06 +0300

    Modify seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,6 @@ user2 = User.create(name: 'John', photo: 'https://img.freepik.com/free-vector/my
 
 user3 = User.create(name: 'Balix', photo: 'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=1480&t=st=1674661161~exp=1674661761~hmac=e529680000ea966150e5ea3b38241a0d8c9582faf23dac8af61ce1785cf27838', bio: 'Teacher from Mexico.', posts_counter: 0)
 
-user4 = User.create(name: 'Balik', photo: 'https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=1480&t=st=1674661161~exp=1674661761~hmac=e529680000ea966150e5ea3b38241a0d8c9582faf23dac8af61ce1785cf27838', bio: 'Teacher from Mexico.', posts_counter: 0)
 # Posts
 post1 = Post.create(author: user1, title: 'Hello', text: 'This is my first post', comments_counter: 0, likes_counter: 0)
 
